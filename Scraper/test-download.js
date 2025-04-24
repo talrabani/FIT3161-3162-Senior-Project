6993a48fcdc2c5a2e977a787e7692eb4e6744cc9
Feba <asal0049@student.monash.edu>
2025-04-25 00:26:38 +1000

    changed bom zip downloader

--- Scraper/test-download.js
+++ Scraper/test-download.js
@@ -6,11 +6,11 @@ const http = require('http');
 const readline = require('readline');
 
 // Function to parse stations.txt file and extract station information
-function loadStationsFromFile() {
+function loadStationsFromFile(filename) {
     try {
         const stationJson = {};
-        if (fs.existsSync(path.join(__dirname, 'temp-stations.txt'))) {
-            const fileContent = fs.readFileSync(path.join(__dirname, 'temp-stations.txt'), 'utf8');
+        if (fs.existsSync(path.join(__dirname, filename))) {
+            const fileContent = fs.readFileSync(path.join(__dirname, filename), 'utf8');
             const lines = fileContent.split('\n');
             
             // Skip header lines (first 4 lines)
@@ -31,18 +31,10 @@ function loadStationsFromFile() {
                 // Basic validation: first segment should be 6-7 digit station number
                 const firstSegment = line.substring(0, 7).trim();
                 
-                // console.log(stationIndex[1066]);
-                
-                // if (!firstSegment.match(/^\d{4,7}$/)) {
-                //     console.warn(`Skipping invalid line: ${line.substring(0, 30)}...`);
-                //     continue;
-                // }
                 // Parse station data using fixed width format
                 // Example: 001000 01    KARUNJIE                                    1940    1983 -16.2919  127.1956 .....          WA       320.0       ..     ..
                 try {
                     const stationNum = firstSegment.replace(/\D/g,'');
-                    // console.log(stationNum);
-                    // const stNumEnd = stationNum.slice(-1);
 
                     // Finding the name is tricky as it's variable width
                     let nameEnd = 50; // Approximate position where name ends
@@ -62,11 +54,6 @@ function loadStationsFromFile() {
                     const lon = line.substring(startPos+10, startPos+19).trim();
                     const startYear = line.substring(startPos+23, startPos+28).trim();
                     const endYear = line.substring(startPos+32, startPos+36).trim();
-                    // const source = line.substring(startPos+35, startPos+48).trim() === '.....' ? null : line.substring(startPos+35, startPos+48).trim();
-                    // const state = line.substring(startPos+48, startPos+52).trim();
-                    // const height = line.substring(startPos+52, startPos+62).trim() === '..' ? null : line.substring(startPos+52, startPos+62).trim();
-                    // const barHeight = line.substring(startPos+62, startPos+72).trim() === '..' ? null : line.substring(startPos+62, startPos+72).trim();
-                    // const wmo = line.substring(startPos+72).trim() === '..' ? null : line.substring(startPos+72).trim();
                     
                     stationJson[stationNum] = {
                         stationNo: stationNum,
@@ -74,9 +61,7 @@ function loadStationsFromFile() {
                         startYear: startYear,
                         endYear: endYear,
                         latitude: lat,
-                        longitude: lon,
-                        "Rainfall": null,
-                        // "Temperature": null
+                        longitude: lon
                     };
                     // console.log(stationJson[stationNum]);
                 } catch (parseError) {
@@ -84,19 +69,10 @@ function loadStationsFromFile() {
                     // Continue to next line
                 }
             }
-            
-            // Validate the stations - display warning if something seems off
-            const stationCount = Object.keys(stationJson).length;
-            if (stationCount < 1000) {
-                console.warn(`WARNING: Only found ${stationCount} stations, which seems low. There might be a parsing issue.`);
-                // console.log()
-            } else {
-                console.log(`Loaded ${stationCount} stations from stations.txt`);
-            }
-            
+            console.log(`Loaded ${stationJson.length} stations from stations.txt`);
             return stationJson;
         } else {
-            console.error("stations.txt file not found");
+            console.error(`${filename} file not found`);
             return {};
         }
     } catch (error) {
@@ -104,82 +80,8 @@ function loadStationsFromFile() {
         return {};
     }
 }
-function loadRainfallcValues(numOfStations) {
-    const stationIndex = {};
-    for (i = 0; i < numOfStations; i++) {
-        const ind = `${1000 + i}`;
-        stationIndex[ind] = null;
-    }
-    let intialC = 207991 + 297;
-    for (const stIn of Object.keys(stationIndex)){
-        let cdif;
-        const stInLast = stIn.slice(-1);
-        const stInNum = +(stIn);
-        if (stInLast == '0' || stInLast == '5') {
-            cdif = parseInt(stInNum*0.4 - 1);
-        }
-        if (stInLast == '1' || stInLast == '6') {
-            cdif = parseInt(stInNum*0.4 + 0.6);
-            // console.log(cdif + parseInt(stationNum));
-        }
-        if (stInLast == '2' || stInLast == '7') {
-            cdif = parseInt(stInNum*0.4 - 0.8);
-        }
-        if (stInLast == '3' || stInLast == '8') {
-            cdif = parseInt(stInNum*0.4 - 0.2);
-        }
-        if (stInLast == '4' || stInLast == '9') {
-            cdif = parseInt(stInNum*0.4 + 0.4);
-        }
-        intialC += cdif;
-        stationIndex[stIn] = intialC;
-        // console.log(stationIndex);
-    }
-    return stationIndex;
-}
-function loadRainfallcValues(numOfStations) {
-    const stationIndex = {};
-    for (i = 0; i < numOfStations; i++) {
-        const ind = `${1000 + i}`;
-        stationIndex[ind] = null;
-    }
-    let intialC = 207991 + 297;
-    for (const stIn of Object.keys(stationIndex)){
-        let cdif;
-        const stInLast = stIn.slice(-1);
-        const stInNum = +(stIn);
-        if (stInLast == '0' || stInLast == '5') {
-            cdif = parseInt(stInNum*0.4 - 1);
-        }
-        if (stInLast == '1' || stInLast == '6') {
-            cdif = parseInt(stInNum*0.4 + 0.6);
-            // console.log(cdif + parseInt(stationNum));
-        }
-        if (stInLast == '2' || stInLast == '7') {
-            cdif = parseInt(stInNum*0.4 - 0.8);
-        }
-        if (stInLast == '3' || stInLast == '8') {
-            cdif = parseInt(stInNum*0.4 - 0.2);
-        }
-        if (stInLast == '4' || stInLast == '9') {
-            cdif = parseInt(stInNum*0.4 + 0.4);
-        }
-        intialC += cdif;
-        stationIndex[stIn] = intialC;
-        // console.log(stationIndex);
-    }
-    return stationIndex;
-}
 
-
-
-// Load station data directly from stations.txt
-var stationJson = loadStationsFromFile();
-// Create directories for downloaded data
-const DATA_DIR = path.join(__dirname, 'data');
-const RAINFALL_DIR = path.join(DATA_DIR, 'rainfall');
-const TEMPERATURE_DIR = path.join(DATA_DIR, 'temperature');
-
+const stationJson = loadStationsFromFile('min-temp-stations.txt');
 
 // Function to download files
 function downloadFile(url, destination) {
@@ -227,32 +129,60 @@ function downloadFile(url, destination) {
     });
 }
 
-async function autoDownload(data) {
-    let allStationCodes = Object.keys(data);
-    // console.log(allStationCodes.at(1));
-    const stInd = loadRainfallcValues(300018-1000);
+const firstStNum = 1005;
+const lastStNum = 200869;
+
+let curp_c = 218590;
+function loadp_cValues(numOfStations) {
+    const stationIndex = {};
+    for (i = 0; i < numOfStations; i++) {
+        const ind = `${firstStNum + i}`;
+        stationIndex[ind] = null;
+    }
+    for (const stIn of Object.keys(stationIndex)){
+        let cdif;
+        const stInLast = stIn.slice(-1);
+        const stInNum = +(stIn);
+        if (stInLast == '0' || stInLast == '5') {
+            cdif = parseInt(Math.round(stInNum*0.4 - 1));
+        }
+        if (stInLast == '1' || stInLast == '6') {
+            cdif = parseInt(Math.round(stInNum*0.4 + 0.6));
+        }
+        if (stInLast == '2' || stInLast == '7') {
+            cdif = parseInt(Math.round(stInNum*0.4 - 0.8));
+        }
+        if (stInLast == '3' || stInLast == '8') {
+            cdif = parseInt(Math.round(stInNum*0.4 - 0.2));
+        }
+        if (stInLast == '4' || stInLast == '9') {
+            cdif = parseInt(Math.round(stInNum*0.4 + 0.4));
+        }
+        curp_c += cdif;
+        stationIndex[stIn] = curp_c;
+    }
+    return stationIndex;
+}
+
+async function autoDownload(nccObsCode, joinAtDir, zipSuffix) {
+    const stInd = loadp_cValues(lastStNum);
     let stationsDownloaded = 0;
-    for (const stationCode of allStationCodes) {
-        // if (stationCode == allStationCodes.at(300)) {
-        //     break
-        // }
+    for (const stationCode of Object.keys(stationJson)) {
         
-        // Define file paths
-        const rainDestination = path.join(RAINFALL_DIR, `${stationCode}_rainfall.zip`);
+        // Define file path
+        const fileDestination = path.join(joinAtDir, `${stationCode}` + zipSuffix);
         
         // Check if files already exist and skip if they do
-        if (fs.existsSync(rainDestination)) {
+        if (fs.existsSync(fileDestination)) {
             console.log(`  Skipping ${stationCode} - files already exist`);
-            data[stationCode]["Rainfall"] = `file://${rainDestination}`;
             continue;
         }
 
-        if (!fs.existsSync(rainDestination)) {
-            // console.log();
-            const hrefLink = `http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_display_type=dailyZippedDataFile&p_stn_num=${stationCode}&p_c=-${stInd[stationCode]}&p_nccObsCode=136&p_startYear=${stationJson[stationCode].startYear}`
-            // console.log(hrefLink);
+        if (!fs.existsSync(fileDestination)) {
+            const hrefLink = `http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_display_type=dailyZippedDataFile&p_stn_num=${stationCode}&p_c=-${stInd[stationCode]}&p_nccObsCode=${nccObsCode}&p_startYear=${stationJson[stationCode].startYear}`
             console.log(`Downloading data from station ${stationCode}`);
-            await downloadFile(hrefLink,rainDestination)
+            console.log(`p_stn_num=${stationCode}&p_c=-${stInd[stationCode]}&p_nccObsCode=${nccObsCode}&p_startYear=${stationJson[stationCode].startYear}`)
+            await downloadFile(hrefLink,fileDestination)
             stationsDownloaded++;
         }
     }
@@ -262,9 +192,8 @@ async function autoDownload(data) {
 // Run the scraper with user input
 (async () => {
     try {
-        await autoDownload(stationJson);
-        // console.log("Downloading file");
-        // await downloadFile("http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_display_type=dailyZippedDataFile&p_stn_num=2066&p_c=-862062&p_nccObsCode=136&p_startYear=1993",path.join(RAINFALL_DIR, '002066_rainfall.zip'))
+        const dir = path.join(__dirname, 'min-temp')
+        await autoDownload(123, dir, '_min-temp.zip');
 
     } catch (error) {
         console.error("Error using:", error);
